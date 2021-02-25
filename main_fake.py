6b0ed280e5836e017874e6b4202c3e0189b5c44e
bes1337 <cauze4concern@yandex.com>
2021-02-25 18:29:39 +0700

    data parsing

--- main_fake.py
+++ main_fake.py
@@ -22,6 +22,7 @@ T_RSSI = 3
 
 def send(jsn):
     out = b''
+    print(jsn)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
     s.settimeout(1)
@@ -53,32 +54,9 @@ def parse(msg_p, alen):
         ret['tag_id'] = int(msg_p[6 + alen]+msg_p[7 + alen], 16)
     return ret
 
-with serial.Serial(SERIALPORT, BAUDRATE, timeout=0) as ser:
-    while (1):
-        sleep(DELAY)
-        msg_p = []
-        msglen = b''
-        msg = []
-        while (ser.inWaiting() > 0):
-            out = (ser.read(1)).hex()
-            if (len(out) and not msglen):
-                msglen = out
-            msg.append(out)
-        if (len(msg)):
-            ret = {}
-            msg_p = msg[0:int(msg[0])+1]
-            alen = 0
-            blen = len(msg)
-            print(msg[alen])
-            print(msg)
-            print(f"blen {blen}")
-            while True:
-                print(f"aa {alen}")
-                if (alen >= blen):
-                    print("da")
-                    break
-                jsn = json.dumps(parse(msg,alen))
-                print(jsn) 
-                alen = alen + int(msg[alen], 16) + 1
-                send(jsn)
+while True:
+    with open('json.json', "r") as f:
+        for line in f:
+            send(line)
+            sleep(1)
 
