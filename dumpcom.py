65a625b425be2aae5a7ac28213ff54802bba00c2
bes1337 <cauze4concern@yandex.com>
2021-03-26 13:17:39 +0700

    new rtls_all type

--- dumpcom.py
+++ dumpcom.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3 
 #coding: utf-8
 
-import serial, json, threading, sys, os, glob
-import time
+import serial, json, threading, sys, glob
 from time import sleep
 
 import select
@@ -14,20 +13,25 @@ import binascii
 #HOST = '127.0.0.1' 
 HOST = '192.168.36.137' 
 
+# TCP timeout
+TIMEOUT = 1
+
 # serial/com
-SERIALPORT = '/dev/ttyACM0'
-BAUDRATE = 115200
-DELAY = 0.0001
+SERIALPORT  = '/dev/ttyACM0'
+BAUDRATE    = 115200
+DELAY       = 0.0001
 
 # defines
-T_PULSE = 1
-T_PRESSURE = 2
-T_RSSI = 3
+T_PULSE         = 1
+T_PRESSURE      = 2
+T_SATURATION    = 4
+T_ALL           = 5
+T_RSSI          = 3
 
 send_queue = queue.Queue()
 
 class Dumpcom:
-    def __init__(self, serialport: str, baudrate: int, delay: float):
+    def __init__(self, serialport, baudrate=BAUDRATE, delay=DELAY):
         self.serialport = serialport
         self.baudrate = baudrate
         self.delay = delay
@@ -38,16 +42,20 @@ class Dumpcom:
         self.com.reset_input_buffer()
 
     def __del__(self):
+        self.listen_com_stop()
         self.com.close()
 
     def listen_com(self):
         self.listen_com_thread = True
-        # logging.info("UART console saving to file: " + self.logfilename)
         self.thread = threading.Thread(target=self._listen, args=())
         self.thread.start()
 
     def listen_com_stop(self):
+        send_queue.put(None)
         self.listen_com_thread = False
+
+    def listen_com_is_running(self):
+        return self.listen_com_thread
 
     def make_json(self, msg: dict) -> str:
         return json.dumps(msg)
@@ -70,6 +78,15 @@ class Dumpcom:
                 ret['tag_id']        = (line[3] << 8)+(line[4])
                 ret['pressure_up']   = line[5]
                 ret['pressure_down'] = line[6]
+            elif (line[2] == T_SATURATION and len(line) == 6):
+                ret['tag_id']        = (line[3] << 8)+(line[4])
+                ret['saturation']         = line[5]
+            elif (line[2] == T_ALL and len(line) == 9):
+                ret['tag_id']        = (line[3] << 8)+(line[4])
+                ret['pulse']         = line[5]
+                ret['saturation']    = line[6]
+                ret['pressure_up']   = line[7]
+                ret['pressure_down'] = line[8]
             elif (line[2] == T_RSSI and len(line) == 8):
                 ret['beacon_id']     = (line[3] << 8)+(line[4])
                 ret['rssi']          = line[5] - (1 << 8) # if line[5] & (1 << (8-1)):
@@ -78,7 +95,7 @@ class Dumpcom:
                 buff.append(ret)
         return buff
 
-    def _make_lines(self, lines: list, unique=False):
+    def _make_lines(self, lines: list, unique=True):
         if (lines is None):
             return
         buff = []
@@ -92,7 +109,6 @@ class Dumpcom:
 
     def _listen(self):
         while (self.listen_com_thread):
-            #data = self._read_com()
             for pkt in self._read_com_yi():
                 #print(f"data: {pkt}")
                 if len(pkt) < 2:
@@ -120,7 +136,12 @@ class Dumpcom:
         tmp = bytearray([])
         tmp2 = b''
         while True:
-            tmp += bytearray(self.com.read())
+            try:
+                tmp += bytearray(self.com.read())
+            except serial.serialutil.SerialException as e:
+                print(f"lost connection with com-device: {e}")
+                self.listen_com_stop()
+                break
             tmp_len = len(tmp)
             if tmp_len > 0:
                 #print(f"[{tmp}]")
@@ -145,18 +166,6 @@ class Dumpcom:
         if (len(buff) > 0):
             return buff         
 
-def send(soc, jsn):
-    if (jsn == 'null' or jsn == '{}'):
-        return
-    out = b''
-    try:
-        #soc.settimeout(3)
-        soc.sendall( jsn.encode() )
-    except socket.error as err:
-        print(f'{err}')
-    #finally:
-    #   soc.close()
-
 if __name__ == '__main__':
     ser = SERIALPORT
     host = HOST
@@ -170,11 +179,11 @@ if __name__ == '__main__':
         exit()
     
     #serial thread for UART listener and parser
-    com = Dumpcom(ser, BAUDRATE, DELAY)
+    com = Dumpcom(ser)
     com.listen_com()
 
     #main thread for tcp connections
-    while (True):
+    while (com.listen_com_is_running()):
         a = send_queue.get()
         if (a):
             if (a == 'null' or a == '{}'):
@@ -182,7 +191,7 @@ if __name__ == '__main__':
             try:
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:   
                     sock.setblocking(False)
-                    sock.settimeout(3)
+                    sock.settimeout(TIMEOUT)
                     sock.connect(( host, 9090))
                     #print(f"sending {a}")
                     sock.sendall( a.encode() )
